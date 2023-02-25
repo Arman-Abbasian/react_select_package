811a60a643a5a7bfec554e23e3a056dd44fdf73a
Arman Abbasian <105098083+Arman-Abbasian@users.noreply.github.com>
2023-02-25 10:36:54 +0330

    Finish work on Sync1 and make Async1 component

--- src/components/Sync1.jsx
+++ src/components/Sync1.jsx
@@ -1,17 +1,50 @@
 import { useState } from 'react';
 import Select from 'react-select';
+import makeAnimated from 'react-select/animated'
 const options=[
-    {id:1,label:"cover 405",value:"cover 405"},
-    {id:2,label:"cover 2008",value:"cover 2008"},
-    {id:3,label:"cover 206",value:"cover 206"},
+    {id:1,label:"cover 405",value:"cover 405",color:"green"},
+    {id:2,label:"cover 2008",value:"cover 2008",color:"blue"},
+    {id:3,label:"cover 206",value:"cover 206",color:"red"},
 ];
 const Sync1 = () => {
-    const [selectedValue,setSelectedValue]=useState('')
+    const [selectedValue,setSelectedValue]=useState([])
     const handleChange=(e)=>{
         console.log(e)
+        setSelectedValue(e)
     }
+    const animation=makeAnimated();
+    const styles={
+            option:(styles,data)=>{
+                return{
+                    ...styles,
+                    color:"red",
+                    backgroundColor:`${data.isFocused?data.data.color:'white'}`,
+                    ':hover':{backgroundColor:data.data.color,color:'white'}, 
+                }
+            },
+                multiValue:(styles,data)=>{
+                    console.log(data)
+                    return{
+                        ...styles,
+                        color:'red',
+                        backgroundColor:`${data.isFocused?'green':'white'}`,
+                        ':hover':{backgroundColor:data.data.color,color:'white'}, 
+                    }
+                },
+                multiValueLabel:(styles,data)=>{
+                    console.log(data)
+                    return{
+                        ...styles,
+                        color:data.data.color,
+                        cursor:"pointer",
+                        ':hover':{backgroundColor:data.data.color,color:'white'},
+                        backgroundColor:`${data.isFocused?data.data.color:'white'}`, 
+                    }
+                }
+            }
+    
     return ( 
-        <Select onChange={handleChange} value={selectedValue} options={options} />
+        <Select onChange={handleChange} value={selectedValue} options={options} styles={styles} placeholder="select" isMulti components={animation}/>
      );
 }
  
